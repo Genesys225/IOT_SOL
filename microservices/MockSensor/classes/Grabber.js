06fb0a5c4eb5129cb459c24d6efcd5ccad4eae94
Alexander <alexanderti@gobrowsi.com>
2020-08-28 17:23:10 +0300

    mock server

---
+++ microservices/MockSensor/classes/Grabber.js
@@ -0,0 +1,8 @@
+class Grabber {
+  async hello(xxx) {
+    console.log("hello");
+    return xxx;
+  }
+}
+
+module.exports = Grabber;
---
+++ microservices/MockSensor/main.js
@@ -0,0 +1,25 @@
+const Grabber = require("./classes/Grabber");
+const Mqtt = require("exec-mqtt");
+const mqtt = require("mqtt")
+
+const grabber = new Grabber();
+const url = "mqtt://emqtt";
+const port = 1883;
+const clientConnectionParams = {
+  services: { grabber },
+  name: "mockDevice",
+  mqttSetting: { url, port },
+};
+const mockDevice = mqtt.connect(url)
+mockDevice.on('connect', function () {
+  mockDevice.subscribe('sensors/#', function (err) {
+    if (!err) {
+      setInterval(() => {
+     
+        mockDevice.publish('SOL-11:11:11:11:11:11/temp', Math.floor(Math.random() * 10).toString())
+      }, 1000);
+     
+    }
+  })
+})
+  
