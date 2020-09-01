df243586e24c1f8d203509b1453d4cf162757abb
gene <eugene@tohen-media.com>
2020-09-01 21:48:09 +0300

    to 5000

--- microservices/db/main.js
+++ microservices/db/main.js
@@ -1,30 +1,30 @@
-const HelloWorld = require("./classes/HelloWorld");
-const mqtt = require("exec-mqtt");
-const Mysql = require("./classes/Mysql");
+const HelloWorld = require('./classes/HelloWorld');
+const mqtt = require('exec-mqtt');
+const Mysql = require('./classes/Mysql');
 
 const helloWorld = new HelloWorld();
 const mysql = new Mysql();
 
 const clientConnectionParams = {
-  services: { helloWorld, mysql },
-  name: "db",
-  mqttSetting: {
-    url: "mqtt://emqtt",
-    port: 1883,
-  },
+	services: { helloWorld, mysql },
+	name: 'db',
+	mqttSetting: {
+		url: 'mqtt://emqtt',
+		port: 1883,
+	},
 };
 
 const mqttClient = new mqtt(clientConnectionParams);
 
 mqttClient.init().then((client) => {
-  mqttClient
-    .exec(
-      "Grabber/grabber.hello",
-      { uri: "XXXXXXXXXXXXXXXXXXXXXX" },
-      { timeout: 1000 }
-    )
-    .then((res) => {
-      console.log(res);
-      return res
-    });
+	mqttClient
+		.exec(
+			'Grabber/grabber.hello',
+			{ uri: 'XXXXXXXXXXXXXXXXXXXXXX' },
+			{ timeout: 5000 }
+		)
+		.then((res) => {
+			console.log(res);
+			return res;
+		});
 });
