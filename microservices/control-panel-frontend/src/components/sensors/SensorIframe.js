73a12007b0afded81555bedaba73e4da6f4766a5
gene <eugene@tohen-media.com>
2020-09-19 01:00:28 +0300

    detect iframe click

--- microservices/control-panel-frontend/src/components/sensors/SensorIframe.js
+++ microservices/control-panel-frontend/src/components/sensors/SensorIframe.js
@@ -1,18 +1,57 @@
-import React, { useState } from 'react';
+import { Box, makeStyles } from '@material-ui/core';
+import React, { useEffect, useState } from 'react';
+
+const useStyles = makeStyles({
+	iframeWrap: {
+		cursor: 'pointer',
+	},
+});
 
 function SensorIframe(props) {
+	const [iframeHover, setIframeHover] = useState(false);
+	const classes = useStyles();
 	const [time, setTime] = useState(Date.now());
 	const panelId = hashCode(props.id);
 	const parentBoundingRect = props.listItemRef.current.getBoundingClientRect();
 	const timePeriod = props.timePeriod || '6h';
+
+	function iframeClickHandler(event) {
+		if (iframeHover) {
+			event.preventDefault();
+			console.log('Wow! Iframe Click!');
+		}
+	}
+	useEffect(() => {
+		window.addEventListener('blur', iframeClickHandler);
+		return () => {
+			window.removeEventListener('blur', iframeClickHandler);
+		};
+	}, [iframeHover]);
+
+	const handleIFrameHover = () => {
+		setIframeHover(true);
+	};
+	const handleIFrameOut = () => {
+		document.activeElement.blur();
+		window.top.focus();
+		setIframeHover(false);
+	};
 	return (
-		<iframe
-			src={`http://localhost:3000/d-solo/All/all?orgId=1&refresh=25s&from=now-${timePeriod}&to=now&theme=light&kiosk&panelId=${panelId}`}
-			width={parentBoundingRect.width - 30}
-			height="300"
-			frameBorder="0"
-			title="grafana"
-		></iframe>
+		<div
+			className={classes.iframeWrap}
+			onClick={(event) => event.preventDefault()}
+			onMouseOver={handleIFrameHover}
+			onMouseOut={handleIFrameOut}
+		>
+			<iframe
+				className={classes.iframeWrap}
+				src={`http://localhost:3000/d-solo/All/all?orgId=1&refresh=25s&from=now-${timePeriod}&to=now&theme=light&kiosk&panelId=${panelId}`}
+				width={parentBoundingRect.width - 30}
+				height="300"
+				frameBorder="0"
+				title="grafana"
+			></iframe>
+		</div>
 	);
 }
 function hashCode(s) {
