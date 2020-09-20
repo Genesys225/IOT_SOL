4e37ed24ef9f51b7239c5715919b119fa2bdc81f
gene <eugene@tohen-media.com>
2020-09-20 16:37:08 +0300

    scheduler day, week month view + grafana panel plugins

--- microservices/control-panel-frontend/src/components/Scheduler/Scheduler.js
+++ microservices/control-panel-frontend/src/components/Scheduler/Scheduler.js
@@ -1,231 +1,271 @@
-import React, { useReducer } from 'react';
+import React, { useReducer, useState } from 'react';
 import Paper from '@material-ui/core/Paper';
 import { ViewState, EditingState } from '@devexpress/dx-react-scheduler';
 import {
-  Scheduler, Resources,
-  Appointments,
-  AppointmentForm,
-  AppointmentTooltip,
-  WeekView,
-  EditRecurrenceMenu,
-  AllDayPanel,
-  ConfirmationDialog,
+	Scheduler,
+	Resources,
+	Appointments,
+	AppointmentForm,
+	AppointmentTooltip,
+	WeekView,
+	EditRecurrenceMenu,
+	AllDayPanel,
+	ConfirmationDialog,
+	MonthView,
+	DayView,
+	ViewSwitcher,
+	Toolbar,
 } from '@devexpress/dx-react-scheduler-material-ui';
 // import { appointments } from './demo-data/appointments';
-import { useStyles } from '../hooks/useStyles';
-import { Select, MenuItem } from '@material-ui/core';
+import { Select, MenuItem, useTheme } from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 
-const appointments = [{
-  title: 'Website Re-Design Plan',
-  startDate: new Date(2018, 5, 25, 12, 35),
-  endDate: new Date(2018, 5, 25, 15, 0),
-  id: 0,
-  members: [1, 3, 5],
-  location: 'Room 1',
-}, {
-  title: 'Book Flights to San Fran for Sales Trip',
-  startDate: new Date(2018, 5, 26, 12, 35),
-  endDate: new Date(2018, 5, 26, 15, 0),
-  id: 1,
-  members: [2, 4],
-  location: 'Room 2',
-}, {
-  title: 'Install New Router in Dev Room',
-  startDate: new Date(2018, 5, 27, 12, 35),
-  endDate: new Date(2018, 5, 27, 15, 0),
-  id: 2,
-  members: [3],
-  location: 'Room 3',
-}, {
-  title: 'Approve Personal Computer Upgrade Plan',
-  startDate: new Date(2018, 5, 28, 12, 35),
-  endDate: new Date(2018, 5, 28, 15, 0),
-  id: 3,
-  members: [4, 1],
-  location: 'Room 4',
-}, {
-  title: 'Final Budget Review',
-  startDate: new Date(2018, 5, 29, 12, 35),
-  endDate: new Date(2018, 5, 29, 15, 0),
-  id: 4,
-  members: [5, 1, 3],
-  location: 'Room 5',
-}];
+const useStyles = makeStyles({
+	container: {
+		display: 'flex',
+		marginBottom: (theme) => theme.spacing(2),
+		justifyContent: 'flex-end',
+	},
+	text: (theme) => ({
+		...theme.typography.h6,
+		marginRight: theme.spacing(2),
+	}),
+});
+
+const appointments = [
+	{
+		title: 'Website Re-Design Plan',
+		startDate: new Date(2018, 5, 25, 12, 35),
+		endDate: new Date(2018, 5, 25, 15, 0),
+		id: 0,
+		members: [1, 3, 5],
+		location: 'Room 1',
+	},
+	{
+		title: 'Book Flights to San Fran for Sales Trip',
+		startDate: new Date(2018, 5, 26, 12, 35),
+		endDate: new Date(2018, 5, 26, 15, 0),
+		id: 1,
+		members: [2, 4],
+		location: 'Room 2',
+	},
+	{
+		title: 'Install New Router in Dev Room',
+		startDate: new Date(2018, 5, 27, 12, 35),
+		endDate: new Date(2018, 5, 27, 15, 0),
+		id: 2,
+		members: [3],
+		location: 'Room 3',
+	},
+	{
+		title: 'Approve Personal Computer Upgrade Plan',
+		startDate: new Date(2018, 5, 28, 12, 35),
+		endDate: new Date(2018, 5, 28, 15, 0),
+		id: 3,
+		members: [4, 1],
+		location: 'Room 4',
+	},
+	{
+		title: 'Final Budget Review',
+		startDate: new Date(2018, 5, 29, 12, 35),
+		endDate: new Date(2018, 5, 29, 15, 0),
+		id: 4,
+		members: [5, 1, 3],
+		location: 'Room 5',
+	},
+];
 
 const ResourceSwitcher = ({ mainResourceName, onChange, resources }) => {
-  const classes = useStyles(theme => ({
-    container: {
-      display: 'flex',
-      marginBottom: theme.spacing(2),
-      justifyContent: 'flex-end',
-    },
-    text: {
-      ...theme.typography.h6,
-      marginRight: theme.spacing(2),
-    },
-  }))
-  return (
-    <div className={classes.container}>
-      <div className={classes.text}>
-        Main resource name:
-      </div>
-      <Select
-        value={mainResourceName}
-        onChange={e => onChange(e.target.value)}
-      >
-        {resources.map(resource => (
-          <MenuItem key={resource.fieldName} value={resource.fieldName}>
-            {resource.title}
-          </MenuItem>
-        ))}
-      </Select>
-    </div>
-  )
-}
+	const theme = useTheme();
+
+	const classes = useStyles(theme);
+	return (
+		<div className={classes.container}>
+			<div className={classes.text}>Main resource name:</div>
+			<Select
+				value={mainResourceName}
+				onChange={(e) => onChange(e.target.value)}
+			>
+				{resources.map((resource) => (
+					<MenuItem
+						key={resource.fieldName}
+						value={resource.fieldName}
+					>
+						{resource.title}
+					</MenuItem>
+				))}
+			</Select>
+		</div>
+	);
+};
 
 const reducer = (state, { type, payload = {} }) => {
-  switch (type) {
-    case 'addAlert':
-      const { addedAppointment } = payload
-      return { ...state, addedAppointment }
-    case 'changeAlert':
-      const { appointmentChanges } = payload
-      return { ...state, appointmentChanges }
-    case 'selectEditedAler':
-      const { editingAppointment } = payload
-      return { ...state, editingAppointment }
-    case 'commitAddedAlert':
-      let { data } = state
-      const startingAddedId = data.length > 0 ? data[data.length - 1].id + 1 : 0;
-      data = [...data, { id: startingAddedId, ...payload.added }]
-      return { ...state, data }
-    case 'commitChangesToAlert': {
-      let { data } = state
-      data = data.map(appointment => (
-        payload.changed[appointment.id] ? { ...appointment, ...payload.changed[appointment.id] } : appointment));
-      return { ...state, data }
-    }
-    case 'changeMainResourceName':
-      const { mainResourceName } = payload
-      return { ...state, mainResourceName }
+	switch (type) {
+		case 'addAlert':
+			const { addedAppointment } = payload;
+			return { ...state, addedAppointment };
+		case 'changeAlert':
+			const { appointmentChanges } = payload;
+			return { ...state, appointmentChanges };
+		case 'selectEditedAler':
+			const { editingAppointment } = payload;
+			return { ...state, editingAppointment };
+		case 'commitAddedAlert':
+			let { data } = state;
+			const startingAddedId =
+				data.length > 0 ? data[data.length - 1].id + 1 : 0;
+			data = [...data, { id: startingAddedId, ...payload.added }];
+			return { ...state, data };
+		case 'commitChangesToAlert': {
+			let { data } = state;
+			data = data.map((appointment) =>
+				payload.changed[appointment.id]
+					? { ...appointment, ...payload.changed[appointment.id] }
+					: appointment
+			);
+			return { ...state, data };
+		}
+		case 'changeMainResourceName':
+			const { mainResourceName } = payload;
+			return { ...state, mainResourceName };
 
+		case 'deleteAlert': {
+			let { data } = state;
+			data = data.filter(
+				(appointment) => appointment.id !== payload.deleted
+			);
+			return { ...state, data };
+		}
 
-    case 'deleteAlert': {
-      let { data } = state
-      data = data.filter(appointment => appointment.id !== payload.deleted);
-      return { ...state, data }
-    }
-
-    default:
-      return state;
-  }
-}
+		default:
+			return state;
+	}
+};
 
 const initialState = {
-  data: appointments,
-  currentDate: '2018-06-27',
+	data: appointments,
+	currentDate: '2018-06-27',
 
-  addedAppointment: {},
-  appointmentChanges: {},
-  editingAppointment: undefined,
-  mainResourceName: 'members',
-  resources: [
-    {
-      fieldName: 'location',
-      title: 'Location',
-      instances: [
-        { id: 'Room 1', text: 'Room 1' },
-        { id: 'Room 2', text: 'Room 2' },
-        { id: 'Room 3', text: 'Room 3' },
-        { id: 'Room 4', text: 'Room 4' },
-        { id: 'Room 5', text: 'Room 5' },
-      ],
-    },
-    {
-      fieldName: 'members',
-      title: 'Members',
-      allowMultiple: true,
-      instances: [
-        { id: 1, text: 'Andrew Glover' },
-        { id: 2, text: 'Arnie Schwartz' },
-        { id: 3, text: 'John Heart' },
-        { id: 4, text: 'Taylor Riley' },
-        { id: 5, text: 'Brad Farkus' },
-      ],
-    },
-  ],
+	addedAppointment: {},
+	appointmentChanges: {},
+	editingAppointment: undefined,
+	mainResourceName: 'members',
+	resources: [
+		{
+			fieldName: 'location',
+			title: 'Location',
+			instances: [
+				{ id: 'Room 1', text: 'Room 1' },
+				{ id: 'Room 2', text: 'Room 2' },
+				{ id: 'Room 3', text: 'Room 3' },
+				{ id: 'Room 4', text: 'Room 4' },
+				{ id: 'Room 5', text: 'Room 5' },
+			],
+		},
+		{
+			fieldName: 'members',
+			title: 'Members',
+			allowMultiple: true,
+			instances: [
+				{ id: 1, text: 'Andrew Glover' },
+				{ id: 2, text: 'Arnie Schwartz' },
+				{ id: 3, text: 'John Heart' },
+				{ id: 4, text: 'Taylor Riley' },
+				{ id: 5, text: 'Brad Farkus' },
+			],
+		},
+	],
 };
 
 const SchedulerComp = (props) => {
-  const [state, dispatch] = useReducer(reducer, initialState)
-  const commitChanges = ({ added, changed, deleted }) => {
-    if (added) {
-      dispatch({ type: 'commitAddedAlert', payload: { added } })
-    }
-    if (changed) {
-      dispatch({ type: 'commitChangesToAlert', payload: { changed } })
-    }
-    if (deleted !== undefined) {
-      dispatch({ type: 'deleteAlert', payload: { deleted } })
-    }
-  }
+	const [state, dispatch] = useReducer(reducer, initialState);
+	const [currentView, setCurrentView] = useState('Week');
+	const commitChanges = ({ added, changed, deleted }) => {
+		if (added) {
+			dispatch({ type: 'commitAddedAlert', payload: { added } });
+		}
+		if (changed) {
+			dispatch({ type: 'commitChangesToAlert', payload: { changed } });
+		}
+		if (deleted !== undefined) {
+			dispatch({ type: 'deleteAlert', payload: { deleted } });
+		}
+	};
 
-  const changeMainResource = (mainResourceName) => {
-    dispatch({ type: 'changeMainResourceName', payload: { mainResourceName } })
-  }
+	const changeMainResource = (mainResourceName) => {
+		dispatch({
+			type: 'changeMainResourceName',
+			payload: { mainResourceName },
+		});
+	};
 
-  const {
-    currentDate, data, addedAppointment, appointmentChanges, editingAppointment, resources, mainResourceName
-  } = state;
-  return (
-    <>
-      <ResourceSwitcher
-        resources={resources}
-        mainResourceName={mainResourceName}
-        onChange={changeMainResource}
-      />
-      <Paper>
-        <Scheduler
-          data={data}
-          height={660}
-        >
-          <ViewState
-            currentDate={currentDate}
-          />
-          <EditingState
-            onCommitChanges={commitChanges}
+	const currentViewChange = (currentViewName) => {
+		setCurrentView(currentViewName);
+	};
 
-            addedAppointment={addedAppointment}
-            onAddedAppointmentChange={(addedAppointment) => dispatch({ type: 'addALert', addedAppointment })}
+	const {
+		currentDate,
+		data,
+		addedAppointment,
+		appointmentChanges,
+		editingAppointment,
+		resources,
+		mainResourceName,
+	} = state;
+	return (
+		<>
+			<ResourceSwitcher
+				resources={resources}
+				mainResourceName={mainResourceName}
+				onChange={changeMainResource}
+			/>
+			<Paper>
+				<Scheduler data={data} height={660}>
+					<ViewState
+						defaultCurrentDate={currentDate}
+						currentViewName={currentView}
+						onCurrentViewNameChange={currentViewChange}
+					/>
+					<EditingState
+						onCommitChanges={commitChanges}
+						addedAppointment={addedAppointment}
+						onAddedAppointmentChange={(addedAppointment) =>
+							dispatch({ type: 'addALert', addedAppointment })
+						}
+						appointmentChanges={appointmentChanges}
+						onAppointmentChangesChange={(appointmentChanges) =>
+							dispatch({
+								type: 'changeAlert',
+								appointmentChanges,
+							})
+						}
+						editingAppointment={editingAppointment}
+						onEditingAppointmentChange={(editingAppointment) =>
+							dispatch({
+								type: 'selectEditedAlert',
+								editingAppointment,
+							})
+						}
+					/>
+					<WeekView />
+					<MonthView />
+					<DayView />
+					<Toolbar />
+					<ViewSwitcher />
+					<AllDayPanel />
+					<EditRecurrenceMenu />
+					<ConfirmationDialog />
+					<Appointments />
+					<AppointmentTooltip showOpenButton showDeleteButton />
+					<AppointmentForm />
+					<Resources
+						data={resources}
+						mainResourceName={mainResourceName}
+					/>
+				</Scheduler>
+			</Paper>
+		</>
+	);
+};
 
-            appointmentChanges={appointmentChanges}
-            onAppointmentChangesChange={(appointmentChanges) => dispatch({ type: 'changeAlert', appointmentChanges })}
-
-            editingAppointment={editingAppointment}
-            onEditingAppointmentChange={(editingAppointment) => dispatch({ type: 'selectEditedAlert', editingAppointment })}
-          />
-          <WeekView
-            startDayHour={9}
-            endDayHour={17}
-          />
-          <AllDayPanel />
-          <EditRecurrenceMenu />
-          <ConfirmationDialog />
-          <Appointments />
-          <AppointmentTooltip
-            showOpenButton
-            showDeleteButton
-          />
-          <AppointmentForm />
-          <Resources
-            data={resources}
-            mainResourceName={mainResourceName}
-          />
-        </Scheduler>
-      </Paper>
-    </>
-  );
-
-}
-
-export default SchedulerComp
+export default SchedulerComp;
---
+++ tmp/config/plugins/aceiot-svg-panel/src/options.tsx
@@ -0,0 +1,318 @@
+import React from 'react';
+import { PanelOptionsEditorBuilder, GrafanaTheme, PanelOptionsEditorProps } from '@grafana/data';
+import Editor from '@monaco-editor/react';
+
+import { css } from 'emotion';
+import { config } from '@grafana/runtime';
+import { ACESVGOptions, SVGIDMapping } from './types';
+import { props_defaults } from 'examples';
+// import { Input, stylesFactory, Icon, HorizontalGroup, Label, VerticalGroup, useTheme } from '@grafana/ui';
+import { Button, Tooltip, Input, stylesFactory, HorizontalGroup, Label, VerticalGroup } from '@grafana/ui';
+
+interface MonacoEditorProps {
+  value: string;
+  theme: string;
+  language: string;
+  onChange: (value?: string | undefined) => void;
+}
+class MonacoEditor extends React.PureComponent<MonacoEditorProps> {
+  getEditorValue: any | undefined;
+  editorInstance: any | undefined;
+
+  onSourceChange = () => {
+    this.props.onChange(this.getEditorValue());
+  };
+  onEditorDidMount = (getEditorValue: any, editorInstance: any) => {
+    this.getEditorValue = getEditorValue;
+    this.editorInstance = editorInstance;
+  };
+  updateDimensions() {
+    this.editorInstance.layout();
+  }
+  render() {
+    const source = this.props.value;
+    if (this.editorInstance) {
+      this.editorInstance.layout();
+    }
+    return (
+      <div onBlur={this.onSourceChange}>
+        <Editor
+          height={'33vh'}
+          language={this.props.language}
+          theme={this.props.theme}
+          value={source}
+          editorDidMount={this.onEditorDidMount}
+        />
+      </div>
+    );
+  }
+}
+
+interface SVGIDMappingProps {
+  value: SVGIDMapping;
+  index?: number;
+  styles?: any;
+  onChangeItem?: (a: SVGIDMapping, b: number) => void | undefined;
+  onAdd?: (a: SVGIDMapping) => void;
+  onDelete?: (a: number) => void;
+}
+
+class SvgMapping extends React.PureComponent<SVGIDMappingProps> {
+  constructor(props: SVGIDMappingProps) {
+    super(props);
+    this.state = { ...props.value };
+  }
+  render() {
+    const { value, index, onChangeItem, onAdd, onDelete } = this.props;
+    return (
+      <HorizontalGroup>
+        <Label>SVG ID</Label>
+        <Input
+          type="text"
+          name="svgId"
+          defaultValue={value.svgId}
+          onBlur={e => {
+            const svgId = e.currentTarget.value;
+            this.setState({ svgId: svgId });
+            onChangeItem && index && onChangeItem({ ...value, svgId: svgId }, index);
+          }}
+        />
+        <Label>Mapped Name</Label>
+        <Input
+          type="text"
+          name="mappedName"
+          defaultValue={value.mappedName}
+          onBlur={e => {
+            const mappedName = e.currentTarget.value;
+            this.setState({ mappedName: mappedName });
+            onChangeItem && index && onChangeItem({ ...value, mappedName: mappedName }, index);
+          }}
+        />
+        {value.svgId && onDelete && index !== undefined && (
+          <Tooltip content="Delete this mapping" theme={'info'}>
+            <Button
+              variant="destructive"
+              icon="trash-alt"
+              size="sm"
+              onClick={() => {
+                onDelete(index);
+              }}
+            >
+              Remove
+            </Button>
+          </Tooltip>
+        )}
+        {!value.svgId && onAdd && (
+          <Tooltip content="Add a new SVG Element ID to svgmap property mapping manually" theme={'info'}>
+            <Button
+              variant="secondary"
+              size="sm"
+              icon="plus-circle"
+              onClick={() => {
+                onAdd(this.state as SVGIDMapping);
+              }}
+            >
+              Add
+            </Button>
+          </Tooltip>
+        )}
+      </HorizontalGroup>
+    );
+  }
+}
+
+class SvgMappings extends React.PureComponent<PanelOptionsEditorProps<SVGIDMapping[]>> {
+  onChangeItem = (updatedMapping: SVGIDMapping, index: number) => {
+    let newMappings = [...this.props.value];
+    newMappings[index] = updatedMapping;
+    this.props.onChange(newMappings);
+  };
+  onAdd = (newMapping: SVGIDMapping) => {
+    if (newMapping.svgId !== '') {
+      let newMappings = [...this.props.value, newMapping];
+      this.props.onChange(newMappings);
+    }
+  };
+  onDelete = (index: number) => {
+    let newMappings = [...this.props.value];
+    newMappings.splice(index, 1);
+    this.props.onChange(newMappings);
+  };
+  render() {
+    const styles = getStyles(config.theme);
+    const svgMappings = this.props.value;
+    return (
+      <VerticalGroup>
+        <HorizontalGroup>
+          <Tooltip content="Clear all SVG Element ID to svgmap property mappings" theme="info">
+            <Button
+              variant="destructive"
+              icon="trash-alt"
+              size="sm"
+              onClick={() => {
+                this.props.onChange([]);
+              }}
+            >
+              Clear All
+            </Button>
+          </Tooltip>
+          <SvgMapping value={{ svgId: '', mappedName: '' }} styles={styles} onAdd={this.onAdd} />
+        </HorizontalGroup>
+        {svgMappings.map((currentMapping: SVGIDMapping, index: number) => {
+          return (
+            <SvgMapping
+              key={currentMapping.svgId}
+              value={currentMapping}
+              index={index}
+              onChangeItem={this.onChangeItem}
+              onDelete={this.onDelete}
+              styles={styles}
+            />
+          );
+        })}
+      </VerticalGroup>
+    );
+  }
+}
+
+export const optionsBuilder = (builder: PanelOptionsEditorBuilder<ACESVGOptions>) => {
+  return builder
+    .addBooleanSwitch({
+      category: ['SVG Document'],
+      path: 'svgAutoComplete',
+      name: 'Enable SVG AutoComplete',
+      description: 'Enable editor autocompletion, optional as it can be buggy on large documents',
+    })
+    .addCustomEditor({
+      category: ['SVG Document'],
+      path: 'svgSource',
+      name: 'SVG Document',
+      description: `Editor for SVG Document, while small tweaks can be made here, we recommend using a dedicated 
+        Graphical SVG Editor and simply pasting the resulting XML here`,
+      id: 'svgSource',
+      defaultValue: props_defaults.svgNode,
+      editor: props => {
+        const grafanaTheme = config.theme.name;
+        return (
+          <MonacoEditor
+            language="xml"
+            theme={grafanaTheme === 'Grafana Light' ? 'vs-light' : 'vs-dark'}
+            value={props.value}
+            onChange={props.onChange}
+          />
+        );
+      },
+    })
+    .addBooleanSwitch({
+      category: ['User JS Render'],
+      path: 'eventAutoComplete',
+      name: 'Enable Render JS AutoComplete',
+      description: 'Enable editor autocompletion, optional as it can be buggy on large documents',
+      defaultValue: true,
+    })
+    .addCustomEditor({
+      category: ['User JS Render'],
+      path: 'eventSource',
+      name: 'User JS Render Code',
+      description: `The User JS Render code is executed whenever new data is available, the root svg document is available as 'svgnode',
+        and elements you've mapped using the SVG Mapping tools below are available as properties on the 'svgmap' object.
+        The Grafana DataFrame is provided as 'data' and the 'options' object can be used to pass values and references between
+        the Render context and the Init context`,
+      id: 'eventSource',
+      defaultValue: props_defaults.eventSource,
+      editor: props => {
+        const grafanaTheme = config.theme.name;
+        return (
+          <MonacoEditor
+            language="javascript"
+            theme={grafanaTheme === 'Grafana Light' ? 'vs-light' : 'vs-dark'}
+            value={props.value}
+            onChange={props.onChange}
+          />
+        );
+      },
+    })
+    .addBooleanSwitch({
+      category: ['User JS Init'],
+      path: 'initAutoComplete',
+      name: 'Enable Init JS AutoComplete',
+      description: 'Enable editor autocompletion, optional as it can be buggy on large documents',
+      defaultValue: true,
+    })
+    .addCustomEditor({
+      category: ['User JS Init'],
+      path: 'initSource',
+      name: 'User JS Init Code',
+      description: `The User JS Init code is executed once when the panel loads, you can use this to define helper functions that 
+        you later reference in the User JS Render code section. The sections have identical execution contexts, and any 
+        JS objects you want to reference between them will need to be attached to the options object as properties`,
+      id: 'initSource',
+      defaultValue: props_defaults.initSource,
+      editor: props => {
+        const grafanaTheme = config.theme.name;
+        return (
+          <MonacoEditor
+            language="javascript"
+            theme={grafanaTheme === 'Grafana Light' ? 'vs-light' : 'vs-dark'}
+            value={props.value}
+            onChange={props.onChange}
+          />
+        );
+      },
+    })
+    .addBooleanSwitch({
+      category: ['SVG Mapping'],
+      path: 'addAllIDs',
+      name: 'Add all SVG Element IDs',
+      description:
+        'Parse the SVG Document for Elements with IDs assigned and automatically add them to the mapping list',
+      defaultValue: false,
+    })
+    .addBooleanSwitch({
+      category: ['SVG Mapping'],
+      path: 'captureMappings',
+      name: 'Enable SVG Mapping on Click',
+      description:
+        'When activated, clicking an element in the panel will attempt to map the clicked element or its nearest parent element with an ID assigned',
+      defaultValue: false,
+    })
+    .addCustomEditor({
+      category: ['SVG Mapping'],
+      id: 'svgMappings',
+      path: 'svgMappings',
+      name: 'SVG Mappings',
+      description:
+        'The SVG ID should match an element in the SVG document with an existing ID tag, the element will be attached to the "svgmap" object in the user code execution contexts as a property using the Mapped Name provided below',
+      defaultValue: props_defaults.svgMappings,
+      editor: SvgMappings,
+    });
+};
+
+const getStyles = stylesFactory((theme: GrafanaTheme) => {
+  return {
+    colorPicker: css`
+      padding: 0 ${theme.spacing.sm};
+    `,
+    inputPrefix: css`
+      display: flex;
+      align-items: center;
+    `,
+    trashIcon: css`
+      color: ${theme.colors.textWeak};
+      cursor: pointer;
+      //
+      &:hover {
+        color: ${theme.colors.text};
+      }
+    `,
+    addIcon: css`
+      color: ${theme.colors.textWeak};
+      cursor: pointer;
+      //
+      &:hover {
+        color: ${theme.colors.text};
+      }
+    `,
+  };
+});
+//
