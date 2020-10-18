692f599dde45d79d8f764f6f60ceb213e23bf778
gene <eugene@tohen-media.com>
2020-10-18 23:58:45 +0300

    working remotly

--- microservices/Grabber/main.js
+++ microservices/Grabber/main.js
@@ -1,88 +1,87 @@
-
 const Mqtt = require('exec-mqtt');
 const mqtt = require('mqtt');
-const NATS = require('nats')
+const NATS = require('nats');
 
-const nc = NATS.connect({url:'nats://nats:4222', json: true })
-
+const nc = NATS.connect({ url: 'nats://nats:4222', json: true });
 
 const url = 'mqtt://emqtt';
 
 const grabberClient = mqtt.connect(url);
 
 const fetch = require('node-fetch');
-class Grabber{
-
-	constructor(){
+class Grabber {
+	constructor() {
 		this.devices = [];
 	}
-	setDevices(devices){
-		this.devices = devices
-	} 
-	async subscribeToDataDevicesStream(){
+	setDevices(devices) {
+		this.devices = devices;
+	}
+	async subscribeToDataDevicesStream() {
 		const grabberClient = await mqtt.connect(url);
 
-		return new Promise((resolve)=>{
+		return new Promise((resolve) => {
 			grabberClient.on('connect', () => {
-				grabberClient.subscribe('sensors/#', (err) =>  {
-					grabberClient.on('message', (topic, message)=> {
-
+				grabberClient.subscribe('sensors/#', (err) => {
+					grabberClient.on('message', (topic, message) => {
 						this.processMessage(topic, message);
-						resolve('connected')
+						resolve('connected');
 					});
-					
 				});
 			});
-		})
+		});
 	}
 
-	async setAllDevices(){
-		const res = await fetch('http://microservices:6000/getRoom')
+	async setAllDevices() {
+		const res = await fetch('http://127.0.0.1:6000/getRoom');
 		const rowRes = await res.json();
-		if(!rowRes.dashboard) return []
-		const devices = rowRes.dashboard.panels.map((r)=>r.uid);
-		this.devices = devices
-	    return devices 
+		if (!rowRes.dashboard) return [];
+		const devices = rowRes.dashboard.panels.map((r) => r.uid);
+		this.devices = devices;
+		return devices;
 	}
-	async processMessage(topic=[], message){
+	async processMessage(topic = [], message) {
 		var deviceData = topic.split('/');
 		var deviceType = deviceData[2];
 		var deviceName = deviceData[1];
-		var systemDeviceId = deviceName + "/" + deviceType;
-		if (this.devices.includes(systemDeviceId)){
-			this.write({id:systemDeviceId, value: message})
-		}else{
-			await this.addDevice(topic)
+		var systemDeviceId = deviceName + '/' + deviceType;
+		if (this.devices.includes(systemDeviceId)) {
+			this.write({ id: systemDeviceId, value: message });
+		} else {
+			await this.addDevice(topic);
 			await this.setAllDevices();
 		}
-	   
 	}
-	write({id, value}) {
-		return new Promise((resolve)=>{
-
-		
-		nc.request('db/mysql.writeDeviceData',{ deviceId:id, value: value.toString() } ,(msg) => {
-			//console.log('Device was registered' + msg);
-			resolve()
-		})
-	})
+	write({ id, value }) {
+		return new Promise((resolve) => {
+			nc.request(
+				'db/mysql.writeDeviceData',
+				{ deviceId: id, value: value.toString() },
+				(msg) => {
+					//console.log('Device was registered' + msg);
+					resolve();
+				}
+			);
+		});
 	}
-	
-	addDevice(id){
-		return new Promise((resolve)=>{
-			nc.request('ControlPanelApi/devicesApi/addNewDevice',{ deviceId:id.replace('sensors/', '') } ,(msg) => {
-				resolve()
-			})
-		})
 
+	addDevice(id) {
+		return new Promise((resolve) => {
+			nc.request(
+				'ControlPanelApi/devicesApi/addNewDevice',
+				{ deviceId: id.replace('sensors/', '') },
+				(msg) => {
+					resolve();
+				}
+			);
+		});
 	}
 }
 
 const grabber = new Grabber();
 
-(async ()=>{
+(async () => {
 	setTimeout(async () => {
-	console.log("allDevices: ", await grabber.setAllDevices());  
-	await grabber.subscribeToDataDevicesStream(); 
-	}, 1000);  
-})()
+		console.log('allDevices: ', await grabber.setAllDevices());
+		await grabber.subscribeToDataDevicesStream();
+	}, 1000);
+})();
