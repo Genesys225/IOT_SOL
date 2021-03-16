ba38978758235c4b1ed9d9d380c480cd8ebc5ff7
Genesys225 <gene.korn@gmail.com>
2021-03-16 20:52:16 +0200

    add git subdomain

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,7 +5,7 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(sol-iot.tk)
+domains=(sol-iot.tk git.sol-iot.tk)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="gene.korn@gmail.com" # Adding a valid address is strongly recommended
